937d5c423a0dfcb8988a0de3bfc92ded1fc54396
--BOOMShaKaLaKa <695331437@qq.com>
2016-07-19 20:35:35 +0800

    WWWW

---
+++ ssss.h
@@ -0,0 +1,15 @@
+//
+//  ssss.h
+//  InAppRage
+//
+//  Created by Mac on 16/7/19.
+//  Copyright © 2016年 JS1-ZJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ssss : UIView
+
+
+@property(nonatomic ,assign)NSInteger aa;
+@end
---
+++ ssss.m
@@ -0,0 +1,15 @@
+//
+//  ssss.m
+//  InAppRage
+//
+//  Created by Mac on 16/7/19.
+//  Copyright © 2016年 JS1-ZJT. All rights reserved.
+//
+
+#import "ssss.h"
+
+@implementation ssss
+
+
+
+@end
