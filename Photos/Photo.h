6884e5b086f7c826cb992c9383bb06dcfcf3c8b8
r695331437 <r695331437@163.com>
2016-08-08 11:13:41 +0800

    王五  photo文件

---
+++ Photos/Photo.h
@@ -0,0 +1,13 @@
+//
+//  Photo.h
+//  InAppRage
+//
+//  Created by JS1-ZJT on 16/8/8.
+//  Copyright © 2016年 JS1-ZJT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Photo : NSObject
+
+@end
---
+++ Photos/Photo.m
@@ -0,0 +1,13 @@
+//
+//  Photo.m
+//  InAppRage
+//
+//  Created by JS1-ZJT on 16/8/8.
+//  Copyright © 2016年 JS1-ZJT. All rights reserved.
+//
+
+#import "Photo.h"
+
+@implementation Photo
+
+@end
