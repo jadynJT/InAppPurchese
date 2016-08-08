b548472175dd7c7e4ba8d208dde1d9480fce67d8
r695331437 <r695331437@163.com>
2016-08-08 08:49:08 +0800

    李四 add Order

---
+++ Order.h
@@ -0,0 +1,13 @@
+//
+//  Order.h
+//  InAppRage
+//
+//  Created by JS1-ZJT on 16/8/8.
+//  Copyright © 2016年 JS1-ZJT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Order : NSObject
+
+@end
---
+++ Order.m
@@ -0,0 +1,20 @@
+//
+//  Order.m
+//  InAppRage
+//
+//  Created by JS1-ZJT on 16/8/8.
+//  Copyright © 2016年 JS1-ZJT. All rights reserved.
+//
+
+#import "Order.h"
+
+@implementation Order
+
+-(instancetype)init{
+    if (self = [super init]) {
+        NSLog(@"李四 Order");
+    }
+    return self;
+}
+
+@end
