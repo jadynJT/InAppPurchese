45abc0126f28798fdfe23e07a8bc9aef2e2a421f
r695331437 <r695331437@163.com>
2016-08-08 09:28:02 +0800

    王五 add Purchase文件

---
+++ Purchase/Purchase.h
@@ -0,0 +1,13 @@
+//
+//  Purchase.h
+//  InAppRage
+//
+//  Created by JS1-ZJT on 16/8/8.
+//  Copyright © 2016年 JS1-ZJT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Purchase : NSObject
+
+@end
---
+++ Purchase/Purchase.m
@@ -0,0 +1,20 @@
+//
+//  Purchase.m
+//  InAppRage
+//
+//  Created by JS1-ZJT on 16/8/8.
+//  Copyright © 2016年 JS1-ZJT. All rights reserved.
+//
+
+#import "Purchase.h"
+
+@implementation Purchase
+
+-(instancetype)init{
+    if (self = [super init]) {
+        NSLog(@"王五 Purchase");
+    }
+    return self;
+}
+
+@end
